48019f9cf89506f41eb3e47265dd98b01f59876a
ArdonitSaliji <ardonit.1980@gmail.com>
2023-02-17 05:26:31 +0100

    added other certificates

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import { useState } from 'react';
 import './App.css';
 import frontendCert from './assets/frontend-cert.pdf';
 import backendCert from './assets/backend-cert.pdf';
-
+import tsql from './assets/T-SQL.pdf';
+import azure from './assets/M-Azure.pdf';
 import { Routes, Route, BrowserRouter, useSearchParams, useParams } from 'react-router-dom';
 
 function App() {
@@ -30,6 +31,18 @@ function App() {
                                 width='800px'
                                 height='1200px'
                             />
+                        }
+                    />
+                    <Route
+                        path='/t-sql'
+                        element={
+                            <embed className='cert' src={tsql} width='800px' height='1200px' />
+                        }
+                    />
+                    <Route
+                        path='/azure'
+                        element={
+                            <embed className='cert' src={azure} width='800px' height='1200px' />
                         }
                     />
                 </Routes>
